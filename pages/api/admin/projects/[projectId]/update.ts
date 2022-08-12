7d263a6d6c4b4094b2f9132f1741c38623121dac
Roman Timashev <roman@tmshv.ru>
2022-08-12 05:09:52 +0300

    API Projects (#50)
    * initial projects api and prisma schema changes
    
    * prisma lib empty lines routine
    
    * prisma lib declare global to fix typescript strict type error
    
    * move project create to admin subfolder
    
    * common lib array single
    
    * get/put project
    
    * add feature model to prisma
    
    * create feature api
    
    * add missing AdminProjectDto
    
    * fix "Do nothing if title or description is not set" case
    
    * features api changes
    
    * rm account related commented models from prisma
    
    * rm unused comments from feature model
    
    * change mapping of several fields in Project and Feature

---
+++ pages/api/admin/projects/[projectId]/update.ts
@@ -0,0 +1,46 @@
+import prisma from "@/server/prisma";
+import { NewProjectDto } from "@/common/types";
+import { withUser } from "@/server/middlewares/withUser";
+import { Project, ProjectStatus } from "@prisma/client";
+import { single } from "@/common/lib/array";
+import { switchProjectToActiveStatus } from "@/server/app/project";
+
+type Payload = {
+  title?: string
+  description?: string
+}
+
+function mapResponse<T extends { id: string } = Project>(project: T): NewProjectDto {
+  return {
+    id: project.id,
+  }
+}
+
+export default withUser<NewProjectDto>(async (req, res) => {
+  if (req.method !== 'PUT') {
+    return res.status(404).json({
+      error: 'Not found',
+    })
+  }
+
+  const projectId = single(req.query.projectId)
+
+  const payload = req.body as Payload
+  const title = payload.title ?? ''
+  const description = payload.description ?? ''
+
+  const project = await prisma.project.update({
+    where: {
+      id: projectId,
+    },
+    data: {
+      title,
+      description,
+      updatedAt: new Date(),
+    },
+  })
+
+  await switchProjectToActiveStatus(projectId)
+
+  return res.json(mapResponse(project))
+})
