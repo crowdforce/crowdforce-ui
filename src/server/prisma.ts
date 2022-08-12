1ae991442c7aa287aa96543a8ed18605c48b027a
Roman Timashev <roman@tmshv.ru>
2022-08-12 03:15:13 +0300

    prisma lib empty lines routine

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
-//
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
@@ -16,4 +15,5 @@ if (process.env.NODE_ENV === 'production') {
     }
     prisma = global.prisma
 }
+
 export default prisma
