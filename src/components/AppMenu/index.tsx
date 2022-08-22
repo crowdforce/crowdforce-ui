7d6b2adde1d7733f0d44c1b91d9ff3ffeba18853
Roman Timashev <roman@tmshv.ru>
2022-08-22 18:24:12 +0300

    use next link instead of a

--- src/components/AppMenu/index.tsx
+++ src/components/AppMenu/index.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import s from './AppMenu.module.css'
 
 export const AppMenu = () => {
     return (
         <nav className={s.navLinks}>
-            <a href="/wiki">База знаний</a>
-            <a href="/about">О нас</a>
+            <Link href="/wiki" passHref>База знаний</Link>
+            <Link href="/about" passHref>О нас</Link>
         </nav>
     )
 }
