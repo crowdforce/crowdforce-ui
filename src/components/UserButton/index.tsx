8be9a46e06a4bafcde2e13bdffb0a504fdbf2fdb
Roman Timashev <roman@tmshv.ru>
2022-08-22 22:36:08 +0300

    user button svg back

--- src/components/UserButton/index.tsx
+++ src/components/UserButton/index.tsx
@@ -98,6 +98,10 @@ export const UserButton = () => {
                             <Text weight={500} size="sm" sx={{ lineHeight: 1 }} pr={'sm'}>
                                 {session.data?.user?.name!}
                             </Text>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <polyline points="6 9 12 15 18 9" />
+                            </svg>
                         </Group>
                     </UnstyledButton>
                 </Menu.Target>
@@ -106,15 +110,38 @@ export const UserButton = () => {
                     <Menu.Item
                         component='a'
                         href='/profile'
+                        icon={(
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <circle cx="12" cy="7" r="4" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                            </svg>
+                        )}
                     >
                         Профиль
                     </Menu.Item>
                     <Menu.Item
+                        icon={(
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                                <line x1="12" y1="11" x2="12" y2="17" />
+                                <line x1="9" y1="14" x2="15" y2="14" />
+                            </svg>
+                        )}
                         onClick={onNewProject}
                     >
                         Новый проект
                     </Menu.Item>
                     <Menu.Item
+                        icon={(
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                                <path d="M7 12h14l-3 -3m0 6l3 -3" />
+                            </svg>
+                        )}
                         onClick={() => signOut()}
                     >
                         Выйти
