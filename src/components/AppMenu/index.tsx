2f39082e3029c4b29f03e6c14d31ef6baca9f221
Roman Timashev <roman@tmshv.ru>
2022-08-22 18:22:10 +0300

    app menu format

--- src/components/AppMenu/index.tsx
+++ src/components/AppMenu/index.tsx
@@ -2,9 +2,9 @@ import s from './AppMenu.module.css'
 
 export const AppMenu = () => {
     return (
-    <nav className={s.navLinks}>
-        <a href="/wiki">База знаний</a>
-        <a href="/about">О нас</a>
-    </nav>
+        <nav className={s.navLinks}>
+            <a href="/wiki">База знаний</a>
+            <a href="/about">О нас</a>
+        </nav>
     )
 }
