165b5c2e6c257402011753514c28b6d16860c775
Roman Timashev <roman@tmshv.ru>
2022-08-23 19:15:00 +0300

    use next link in profile and user menu

--- src/components/UserButton/index.tsx
+++ src/components/UserButton/index.tsx
@@ -3,6 +3,7 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import { Avatar, Group, Menu, UnstyledButton, Text, createStyles, Loader } from '@mantine/core';
 import { useRouter } from 'next/router';
 import { NewProjectDto } from '@/common/types';
+import Link from 'next/link';
 
 const useStyles = createStyles((theme) => ({
     user: {
@@ -109,19 +110,20 @@ export const UserButton: React.FC<UserButtonProps> = () => {
                 </Menu.Target>
 
                 <Menu.Dropdown>
-                    <Menu.Item
-                        component='a'
-                        href='/profile'
-                        icon={(
-                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <circle cx="12" cy="7" r="4" />
-                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                            </svg>
-                        )}
-                    >
-                        Профиль
-                    </Menu.Item>
+                    <Link href='/profile' passHref>
+                        <Menu.Item
+                            component='a'
+                            icon={(
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <circle cx="12" cy="7" r="4" />
+                                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                </svg>
+                            )}
+                        >
+                            Профиль
+                        </Menu.Item>
+                    </Link>
                     <Menu.Item
                         icon={(
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
