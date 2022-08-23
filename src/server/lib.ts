4fe9b5d0147c2808406761116edbaa6d554bdc52
Roman Timashev <roman@tmshv.ru>
2022-08-23 18:36:25 +0300

    Profile page api (#55)
    * next _app update
    
    * profile page update

---
+++ src/server/lib.ts
@@ -0,0 +1,16 @@
+import { getToken, GetTokenParams } from "next-auth/jwt"
+
+
+export async function getUserId(ctx: GetTokenParams): Promise<string | null> {
+    const session = await getToken(ctx)
+    if (!session) {
+        return null
+    }
+
+    const userId = session.sub
+    if (!userId) {
+        return null
+    }
+
+    return userId
+}
