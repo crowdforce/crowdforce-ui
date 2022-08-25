295075d7fb6d111a3dc5b9a0b3f4022e11ce246a
Roman Timashev <roman@tmshv.ru>
2022-08-25 13:12:50 +0300

    map page fix height

--- pages/map.tsx
+++ pages/map.tsx
@@ -3,20 +3,25 @@ import { PublicMap } from '@/components/PublicMap'
 import { NextPage } from 'next'
 import useSWR from 'swr'
 import { PublicProjectDto } from '@/common/types';
+import { Box } from '@mantine/core';
 
 const Index: NextPage = () => {
     const { data, error } = useSWR<PublicProjectDto[]>(`/api/projects`)
 
     return (
         <Page>
-            <PublicMap
-                data={data}
-                initialViewState={{
-                    latitude: 59.94670423319895,
-                    longitude: 30.371801220550694,
-                    zoom: 10,
-                }}
-            />
+            <Box sx={{
+                height: 600,
+            }}>
+                <PublicMap
+                    data={data}
+                    initialViewState={{
+                        latitude: 59.94670423319895,
+                        longitude: 30.371801220550694,
+                        zoom: 10,
+                    }}
+                />
+            </Box>
         </Page>
     );
 };
