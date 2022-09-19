06c9d7662cf449cd91fdee3429400f81d2056b1a
Roman Timashev <roman@tmshv.ru>
2022-09-20 02:11:40 +0300

    move mantine theme to ThemeProvider component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import "@/style.css"
 import Head from "next/head"
 import { MapProvider } from "react-map-gl"
 import { SessionProvider } from "next-auth/react"
-import { MantineProvider } from "@mantine/core"
 import { SWRConfig } from "swr"
 import { AppProps } from "next/app"
 import { Session } from "next-auth"
 import { App } from "@/components/App"
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 type Props = {
     session: Session
@@ -37,73 +37,7 @@ export default function MyApp({ Component, pageProps }: AppProps<Props>) {
                 <meta name="theme-color" content="#ECF2F6" />
             </Head>
 
-            <MantineProvider withGlobalStyles withNormalizeCSS
-                theme={{
-                    fontFamily: "Raleway",
-                    lineHeight: 1.2,
-                    shadows: {
-                        lg: "0 4px 8px 2px rgba(10, 60, 30, 0.05), 0px 8px 16px -4px rgba(0, 0, 0, 0.1), 8px 24px 24px 0px rgba(0, 0, 0, 0)",
-                    },
-                    spacing: {
-                        xs: 8,
-                        sm: 10,
-                        md: 16,
-                        lg: 20,
-                        xl: 24,
-                    },
-                    headings: {
-                        fontFamily: "Raleway",
-                        sizes: {
-                            h1: {
-                                fontSize: 73,
-                                fontWeight: 900,
-                                lineHeight: "100%",
-                            },
-                            h2: {
-                                fontSize: 47,
-                                fontWeight: 800,
-                                lineHeight: "100%",
-                            },
-                            h3: {
-                                fontSize: 53,
-                                fontWeight: 800,
-                                lineHeight: "110%",
-                            },
-                            h4: {
-                                fontSize: 24,
-                                fontWeight: 600,
-                                lineHeight: "110%",
-                            },
-                            h5: {
-                                fontSize: 30,
-                                fontWeight: 400,
-                                lineHeight: "100%",
-                            },
-                        },
-                    },
-                    primaryColor: "lime",
-                    defaultRadius: "md",
-                    white: "#ECF2F6",
-                    defaultGradient: { deg: 112, from: "cyan", to: "lime" },
-                    datesLocale: "ru",
-                    components: {
-                        Button: {
-                            styles: {
-                                label: {
-                                    fontWeight: 400,
-                                },
-                            },
-                        },
-                        InputWrapper: {
-                            styles: (theme) => ({
-                                label: {
-                                    paddingBottom: theme.spacing.xs,
-                                },
-                            }),
-                        },
-                    },
-                }}
-            >
+            <ThemeProvider>
                 <SessionProvider session={pageProps.session}>
                     <MapProvider>
                         <SWRConfig value={{
@@ -115,7 +49,7 @@ export default function MyApp({ Component, pageProps }: AppProps<Props>) {
                         </SWRConfig>
                     </MapProvider>
                 </SessionProvider>
-            </MantineProvider>
+            </ThemeProvider>
         </>
     )
 }
---
+++ src/components/ColorThemeSwitch/index.tsx
@@ -0,0 +1,22 @@
+import { ActionIcon, useMantineColorScheme } from "@mantine/core"
+import { IconMoonStars, IconSun } from "@tabler/icons"
+
+export type ColorThemeSwitchProps = {
+
+}
+
+export const ColorThemeSwitch: React.FC<ColorThemeSwitchProps> = props => {
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+    const dark = colorScheme === "dark"
+
+    return (
+        <ActionIcon
+            variant="outline"
+            color={dark ? "yellow" : "blue"}
+            onClick={() => toggleColorScheme()}
+            title="Toggle color scheme"
+        >
+            {dark ? <IconSun size={18} /> : <IconMoonStars size={18} />}
+        </ActionIcon>
+    )
+}
