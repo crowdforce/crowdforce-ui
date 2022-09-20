9b6aa44fbd3c39cc0c895d623a7e00e5532238d7
manhir <53478716+manhir@users.noreply.github.com>
2022-09-20 18:50:58 +0300

    upd /profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,110 +1,127 @@
-import { useSession } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react"
 import Page from "@/components/Page"
-import { Avatar, Card, createStyles, Group, Stack, Title } from "@mantine/core"
+import { Avatar, createStyles, Group, Stack, Title, Text, Button, SimpleGrid } from "@mantine/core"
 import { GetServerSideProps, NextPage } from "next"
 import { getUserId } from "@/server/lib"
 import { getProjects, ProfileResponseDto } from "@/server/controllers/profile"
 import { ProjectCard } from "@/components/ProjectCard"
+import { ProjectAddCard } from "@/components/ProjectAddCard"
+import { ProfileTasks } from "@/components/ProfileTasks"
+import { Task } from "@prisma/client"
 
 const useStyles = createStyles((theme) => ({
-    bigGroup: {
-        alignItems: "flex-start",
-        [theme.fn.smallerThan("sm")]: {
-            flexDirection: "column",
-            alignItems: "stretch",
-            "& div": {
-                maxWidth: "unset",
-            },
-        },
+    section: {
+        width: "100%",
+        maxWidth: 1160,
     },
-    generalInfo: {
-        flexGrow: "initial",
-        marginRight: "auto",
-        [theme.fn.smallerThan("sm")]: {
-            marginRight: "unset",
-        },
-    },
+    logoutButton: {
+        width: "fit-content",
+        minWidth: "min(100%, 200px)",
+    }
 }))
 
 type Props = {
     profile: ProfileResponseDto
+    tasks: Partial<Task>[]
 }
 
-const ProfilePage: NextPage<Props> = props => {
+const ProfilePage: NextPage<Props> = ({ profile }) => {
     const session = useSession()
     const { classes: s } = useStyles()
 
     return (
         <Page>
-            <Group
-                noWrap
-                grow
-                className={s.bigGroup}
+            <Stack
+                align="center"
             >
-                <Card
-                    withBorder
-                    p='lg'
-                    className={s.generalInfo}
+                <Group
+                    noWrap
+                    position="apart"
+                    className={s.section}
                 >
-                    <Group position='center'>
-                        <Stack>
-                            <Avatar src={session.data?.user?.image!} size={256} radius={"50%" as any} />
-                            <Title order={1}>
-                                {session.data?.user?.name!}
-                            </Title>
-                        </Stack>
+                    <Group>
+                        <Avatar
+                            src={session.data?.user?.image!}
+                            size={32}
+                        />
+                        <Text size="sm" sx={{ lineHeight: 1 }}
+
+                        >
+                            {session.data?.user?.name!}
+                        </Text>
                     </Group>
-                </Card>
-                <Card withBorder>
-                    <Card.Section
-                        withBorder
-                        inheritPadding
-                        py='inherit'
+                    <Button
+                        className={s.logoutButton}
+                        onClick={() => signOut()}
                     >
-                        <Title order={2}>
-                            Проекты за которыми вы следите
-                        </Title>
-                    </Card.Section>
-                    <Stack
-                        py='inherit'
-                    >
-                        {props.profile.following.map(({ id, title, description, imageUrl }) => (
-                            <ProjectCard
-                                key={id}
-                                coverSrc={imageUrl}
-                                title={title}
-                                description={description}
-                                href={`/project/${id}`}
-                            />
-                        ))}
-                    </Stack>
-                </Card>
-                <Card withBorder>
-                    <Card.Section
-                        withBorder
-                        inheritPadding
-                        py='inherit'
-                    >
-                        <Title order={2}>
-                            Проекты которые вы курируете
-                        </Title>
-                    </Card.Section>
-                    <Stack
-                        py='inherit'
-                    >
-                        {props.profile.owned.map(({ id, title, description, imageUrl }) => (
-                            <ProjectCard
-                                key={id}
-                                coverSrc={imageUrl}
-                                title={title}
-                                description={description}
-                                href={`/project/${id}`}
-                            />
-                        ))}
-                    </Stack>
-                </Card>
-            </Group>
-        </Page>
+                        Выйти
+                    </Button>
+                </Group>
+
+                <Title
+                    className={s.section}
+                >
+                    Мои проекты
+                </Title>
+                <SimpleGrid
+                    className={s.section}
+                    cols={3}
+                    breakpoints={[
+                        { maxWidth: "xl", cols: 3 },
+                        { maxWidth: "md", cols: 2 },
+                        { maxWidth: "xs", cols: 1 },
+                    ]}
+                >
+                    {profile.following.map(({ id, title, description, imageUrl }) => (
+                        <ProjectCard
+                            key={id}
+                            coverSrc={imageUrl}
+                            title={title}
+                            description={description}
+                            href={`/project/${id}`}
+                        />
+                    ))}
+                    <ProjectAddCard />
+                </SimpleGrid>
+
+                <Title
+                    className={s.section}
+                >
+                    Мои задачи
+                </Title>
+
+                <ProfileTasks
+                    data={[]}
+                />
+
+                <Title
+                    className={s.section}
+                >
+                    Интересные проекты
+                </Title>
+
+                <SimpleGrid
+                    className={s.section}
+                    cols={3}
+                    breakpoints={[
+                        { maxWidth: "xl", cols: 3 },
+                        { maxWidth: "md", cols: 2 },
+                        { maxWidth: "xs", cols: 1 },
+                    ]}
+                >
+                    {/* wrong projects data */}
+                    {profile.following.map(({ id, title, description, imageUrl }) => (
+                        <ProjectCard
+                            key={id}
+                            coverSrc={imageUrl}
+                            title={title}
+                            description={description}
+                            href={`/project/${id}`}
+                        />
+                    ))}
+                </SimpleGrid>
+            </Stack>
+        </Page >
     )
 }
 
@@ -120,10 +137,40 @@ export const getServerSideProps: GetServerSideProps<Props> = async ctx => {
     }
 
     const profile = await getProjects(userId)
+    const tasks = [ // no path in backend
+        {
+            "id": "cl86ovzbb0044pb3krupkrdpz",
+            "title": "Обоссать",
+            "description": "Просим обоссать каждое дерево в парке.",
+            "dateStart": "Thu Sep 15 2022",
+            "timeStart": "Tue Sep 20 2022",
+            "dateEnd": "Thu Sep 15 2022",
+            "timeEnd": "Tue Sep 20 2022",
+            "followers": [
+                {
+                    "id": "cl787cvnz0012jb3kcg23zg4w",
+                    "name": "Roman Timashev",
+                    "image": "https://t.me/i/userpic/320/pyFRk5ueqLuVIiYhmRzZrpv8KOD6n1wqAWgudLRb6Sg.jpg",
+                    "status": "participant",
+                },
+            ],
+        },
+        {
+            "id": "cl86psi2a0316pb3kngazt098",
+            "title": "Добавить навозия",
+            "description": "Навозия очень мало, давайте добавим",
+            "dateStart": "Thu Sep 15 2022",
+            "timeStart": "Tue Sep 20 2022",
+            "dateEnd": "Thu Sep 15 2022",
+            "timeEnd": "Tue Sep 20 2022",
+            "followers": [],
+        },
+    ]
 
     return {
         props: {
             profile,
+            tasks,
         },
     }
 }
