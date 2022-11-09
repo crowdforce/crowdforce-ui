700f37b293dabd8a7e68ab5427ab06bf2644e812
Roman Timashev <roman@tmshv.ru>
2022-11-09 23:42:44 +0600

    rm unused

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Button, Center, Container, createStyles, Group, keyframes, Loader, MediaQuery, SimpleGrid, Stack, Text, Title } from "@mantine/core"
 import { IconCornerLeftDownDouble, IconMouse } from "@tabler/icons"
 import Image from "next/image"
-import Page from "@/components/Page"
 import heroLine from "@/../public/index/heroLine.svg"
 import bigLine from "@/../public/index/bigLine.svg"
 import blueLine from "@/../public/index/blueLine.svg"
