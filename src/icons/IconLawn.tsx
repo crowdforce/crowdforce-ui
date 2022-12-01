aeaadf8a91ebd9f1e735d1a3b4ded5edb1f5e410
Roman Timashev <roman@tmshv.ru>
2022-12-01 18:14:40 +0600

    add icons

---
+++ src/icons/IconLawn.tsx
@@ -0,0 +1,22 @@
+function IconLawn({
+    size = 24,
+    color = "currentColor",
+    stroke = 2,
+    ...props
+}) {
+    return (
+        <svg width={size} height={size} viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 7.5 4.25 L 5.3333 4.25 C 4.735 4.25 4.25 4.735001 4.25 5.3333 L 4.25 7.5 C 4.25 8.098301 4.735 8.5833 5.3333 8.5833 L 7.5 8.5833 C 8.098301 8.5833 8.5833 8.098301 8.5833 7.5 L 8.5833 5.3333 C 8.5833 4.735001 8.098301 4.25 7.5 4.25 Z" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 22.667 4.25 L 20.500299 4.25 C 19.902 4.25 19.417 4.735001 19.417 5.3333 L 19.417 7.5 C 19.417 8.098301 19.902 8.5833 20.500299 8.5833 L 22.667 8.5833 C 23.265301 8.5833 23.750299 8.098301 23.750299 7.5 L 23.750299 5.3333 C 23.750299 4.735001 23.265301 4.25 22.667 4.25 Z" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 22.667 19.4165 L 20.500299 19.4165 C 19.902 19.4165 19.417 19.901501 19.417 20.4998 L 19.417 22.6665 C 19.417 23.264799 19.902 23.7498 20.500299 23.7498 L 22.667 23.7498 C 23.265301 23.7498 23.750299 23.264799 23.750299 22.6665 L 23.750299 20.4998 C 23.750299 19.901501 23.265301 19.4165 22.667 19.4165 Z" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 7.5 19.4165 L 5.3333 19.4165 C 4.735 19.4165 4.25 19.901501 4.25 20.4998 L 4.25 22.6665 C 4.25 23.264799 4.735 23.7498 5.3333 23.7498 L 7.5 23.7498 C 8.098301 23.7498 8.5833 23.264799 8.5833 22.6665 L 8.5833 20.4998 C 8.5833 19.901501 8.098301 19.4165 7.5 19.4165 Z" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 6.417 8.5835 L 6.417 19.4168" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 21.583 8.5835 L 21.583 19.4168" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 8.583 6.4165 L 19.4163 6.4165" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 8.583 21.5835 L 19.4163 21.5835" />
+            <path fill="none" stroke={color} stroke-width={stroke} stroke-linecap="round" stroke-linejoin="round" d="M 16.999701 15.9998 C 16.204 15.9998 15.441 16.315901 14.878401 16.8785 C 14.315701 17.441099 13.999701 18.204201 13.999701 18.9998 M 16.999701 13.3332 C 16.204 13.3332 15.441 13.6492 14.878401 14.211901 C 14.315701 14.7745 13.999701 15.5375 13.999701 16.3332 M 16.999701 10.6665 C 16.204 10.6665 15.441 10.982599 14.878401 11.5452 C 14.315701 12.1078 13.999701 12.870899 13.999701 13.6665 M 11.333 15.9998 C 12.806299 15.9998 13.999701 17.343201 13.999701 18.9998 M 11.333 13.3332 C 12.806299 13.3332 13.999701 14.6765 13.999701 16.3332 M 11.333 10.6665 C 12.806299 10.6665 13.999701 12.0098 13.999701 13.6665 M 13.999701 8.6665 L 13.999701 12.6665" />
+        </svg>
+    )
+}
+
+export default IconLawn
