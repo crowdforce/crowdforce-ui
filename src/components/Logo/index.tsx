ca7e8f571f00f3faa15da34eb70c412037d3a250
Roman Timashev <roman@tmshv.ru>
2023-01-19 15:30:13 +0600

    update logo; support dark theme

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link"
-import { Badge, Group } from "@mantine/core"
+import { Badge, Group, useMantineTheme } from "@mantine/core"
 import Crowdforce from "./logo.svg"
 
-export const Logo = () => (
-    <Link href={"/"} passHref>
-        <Group
-            noWrap
-            sx={{
-                userSelect: "none",
-                cursor: "pointer",
-                gap: 0,
-            }}
-        >
-            <Crowdforce height={32} />
-            <Badge>Beta</Badge>
-        </Group>
-    </Link>
-)
+export const Logo = () => {
+    const theme = useMantineTheme()
+
+    return (
+        <Link href={"/"} passHref>
+            <Group
+                noWrap
+                sx={{
+                    userSelect: "none",
+                    cursor: "pointer",
+                    gap: 0,
+                }}
+            >
+                <Crowdforce height={32} fill={theme.colorScheme === "dark" ? "#FFFFFF" : "#16171B"} />
+                <Badge>Beta</Badge>
+            </Group>
+        </Link>
+    )
+}
