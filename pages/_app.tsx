9f236ea2d9e1d5427f7273a522704aaaca603640
Roman Timashev <roman@tmshv.ru>
2023-01-19 16:45:29 +0600

    fix favicons; update manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,13 +30,12 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout<Props
         <>
             <Head>
                 <title>Crowd Force</title>
-                <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-                <link rel="icon" href="/assets/favicon.png" sizes="any" />
+                <link rel="icon" type="image/png" href="/assets/favicon.png" sizes="any" />
                 <link rel="icon" href="/assets/favicon.svg" type="image/svg+xml" />
                 <link rel="apple-touch-icon" href="/assets/apple-touch-icon.png" />
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="msapplication-TileColor" content="#ECF2F6" />
-                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+                <meta name="msapplication-TileImage" content="/assets/favicon-192.png" />
                 <meta name="theme-color" content="#ECF2F6" />
             </Head>
 
